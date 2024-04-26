56cdd0adb852a80f5aba8e214c098647883bfffb
Matheus Sanches Jurgensen <mjurgensen@Matheuss-MacBook-Pro.local>
2024-04-25 21:32:35 -0300

    Adds store_predicates() to domain.py
    Co-authored-by: Henri Michel <henrimichel@usp.br>

--- src/domain.py
+++ src/domain.py
@@ -8,10 +8,19 @@ def store_constants(domain, dict_const = {}):
         dict_const[key].append(repr(constant)[9:-1])
     return dict_const
 
+def store_predicates(domain, dict_predicates = {}):
+    for predicate in domain.predicates:
+        dict_predicates[predicate.name] = []
+        for object in predicate.terms:
+            dict_predicates[predicate.name].append(next(iter(object.type_tags)))
+    return dict_predicates
+
 def main():
     domain = parse_domain("../tests/gripper3.pddl")
     dict_obj = store_constants(domain)
-    print(dict_obj)
+    print("Objetos:", dict_obj)
+    dict_predicates = store_predicates(domain)
+    print("\nPredicados:", dict_predicates)
 
 if __name__ == "__main__":
     main()
