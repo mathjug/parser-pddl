8b81c11eba8f479e42ddb96446621baffc9e03fd
Henri Michel <henrimichel@usp.br>
2024-05-01 20:24:37 -0300

    Update Predicate.__str__() method

--- src/custom_types.py
+++ src/custom_types.py
@@ -7,9 +7,10 @@ class Object:
         return self.name
 
 class Predicate:
-    def __init__(self, name, type_variables = []):
+    def __init__(self, name, variable_types = []):
         self.name = name
-        self.type_variables = type_variables[:]
+        self.variable_types = variable_types[:]
 
     def __str__(self):
-        return self.name
+        output = self.name + " " + str(self.variable_types)
+        return output
