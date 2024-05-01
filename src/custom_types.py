bf333cd1726357bfba900b6c8d12144b55fa3f04
Henri Michel <henrimichel@usp.br>
2024-05-01 18:56:06 -0300

    Add class Predicate

--- src/custom_types.py
+++ src/custom_types.py
@@ -5,3 +5,11 @@ class Object:
 
     def __str__(self):
         return self.name
+
+class Predicate:
+    def __init__(self, name, type_variables = []):
+        self.name = name
+        self.type_variables = type_variables[:]
+
+    def __str__(self):
+        return self.name
