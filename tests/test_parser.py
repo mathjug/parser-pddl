f6fe59d262408c3d25034d55eba820f4329f74df
João Guilherme <alvesjg16@gmail.com>
2024-05-24 18:26:09 -0300

    add goal_state test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -20,6 +20,17 @@ def test_prepositions_storage(filenames, expected):
 def test_initial_state_storage(filenames, expected):
     parser = Parser(filenames[0],filenames[1])
     initial_state = parser.get_initial_state()
-    ans = sorted([str(parser.propositions[i]) for i in range(len(parser.propositions)) if initial_state[i]])
+    ans = sorted([str(parser.propositions[i]) for i in range(len(parser.propositions)) if initial_state[i] == 1])
+    print(ans)
     assert ans==expected
 
+@pytest.mark.parametrize("filenames,expected", [
+    (["./tests/examples/gripper3.pddl","./tests/examples/gripper3_2_balls.pddl"],
+    [("at-ball_ball1_roomb", 0), ("at-ball_ball2_roomb", 1), ("at-robby_roomb", 1), ("whole_ball1", 1), ("whole_ball2", 1)])
+    ])
+def test_goal_state_storage(filenames, expected):
+    parser = Parser(filenames[0],filenames[1])
+    goal_state = parser.get_goal_state()
+    ans = sorted([(str(parser.propositions[i]), goal_state[i]) for i in range(len(parser.propositions)) if goal_state[i] != -1])
+    print(ans)
+    assert ans==expected
