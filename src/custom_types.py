fa4759efb94948c8228476bd2ca846685ac64cec
Henri Michel <henrimichel@usp.br>
2024-06-03 16:18:04 -0300

    Add class Action, and basic methods

--- src/custom_types.py
+++ src/custom_types.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class Object:
     def __init__(self, name: str, type: str):
         self.name = name
@@ -5,28 +8,28 @@ class Object:
 
     def __str__(self):
         return self.name
-    
+
     def get_name(self):
         return self.name
 
     def get_type(self):
         return self.type
-    
+
     def __lt__(self, other):
         return self.name < other.name
 
 class Predicate:
-    def __init__(self, name: str, variable_types: list = []):
+    def __init__(self, name: str, variable_types: list[str] = []):
         self.name = name
         self.variable_types = variable_types[:]
 
     def __str__(self):
         output = self.name + " " + str(self.variable_types)
         return output
-    
+
     def get_name(self):
         return self.name
-    
+
     def get_variable_types(self):
         return self.variable_types
 
@@ -35,21 +38,21 @@ class Proposition:
     Implementation of a proposition, which is an instantiation of a predicate with the
     objects involved.
     '''
-    def __init__(self, predicate: Predicate, objects: list):
+    def __init__(self, predicate: Predicate, objects: list[Object]):
         self.predicate = predicate
         self.objects = objects
 
     def __str__(self):
         output = self.__get_object_names()
         return output
-    
+
     def __get_object_names(self):
         names = ""
         for object in self.objects:
             names += "_" + object.get_name()
         names = self.predicate.get_name() + names
         return names
-    
+
     def compare_names(self, prop_names: str):
         if prop_names == self.__get_object_names():
             return True
@@ -60,3 +63,19 @@ class Proposition:
 
     def get_objects(self):
         return self.objects
+
+class Action:
+    def __init__(self, name: str, objects_type: list[str], preconditions: list[(Predicate, bool)], effects: list[(Predicate, bool)]) -> None:
+        self.name = name
+        self.objects_type = objects_type[:]
+        self.preconditions = preconditions[:]
+        self.effects = effects[:]
+
+    def objects_type(self) -> list[str]:
+        return self.objects_type
+
+    def get_preconditions(self) -> list[(Predicate, bool)]:
+        return self.preconditions
+
+    def get_effects(self) -> list[(Predicate, bool)]:
+        return self.effects
