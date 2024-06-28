1ab896ec8af50e0f1b00ea8a98a9eef1697a7116
Henri Michel <henrimichel@usp.br>
2024-06-28 16:06:36 -0300

    Update run_ground().

--- src/ground.py
+++ src/ground.py
@@ -7,28 +7,37 @@ def store_initial_queue(initial_state: list[int], propositions: list[Proposition
         frontier_queue.appendleft((propositions[i], initial_state[i]))
     return frontier_queue
 
-def run_ground(initial_state: list[int], propositions: list[Proposition],
+def run_ground(initial_state: list[int], propositions: dict[str, Proposition],
                 pred_to_actions: dict[Predicate, list[Action]]):
     frontier_queue = store_initial_queue(initial_state, propositions)
-    reached = {}
-    used_prop = [False for prop in propositions]
-    
+
+    n = len(initial_state)
+    reached = [-1 for _ in range(2 * n)]
+    for i in range(n):
+        if initial_state[i] == 0:
+            reached[i + n] = 0
+        elif initial_state[i] == 1:
+            reached[i] = 0
+
     while(len(frontier_queue) > 0):
         reached_tuple = frontier_queue.pop()
-        reached_predicate = reached_tuple[0].get_predicate()
-        if reached_predicate not in reached:
-            reached[reached_predicate] = []
-        reached[reached_predicate].append(reached_tuple)
+        reached_proposition = reached_tuple[0]
+        value = reached_tuple[1]
 
+        index = reached_proposition.get_index()
+        if value == 0:
+            reached[n + index] = 1
+        else:
+            reached[index] = 1
+
+        reached_predicate = reached_proposition.get_predicate()
         for action in pred_to_actions[reached_predicate]:
-            # excluir as proposições nas pré-condições que são inválidas devido
-            # à utilização da mesma variável que a proposição que acabou de ser
-            # retirada da fronteira
-            continue
-
-
-
-
+            preconditions = action.get_precondition()
+            for precondition in preconditions:
+                precondition_predicate = precondition[0].get_predicate()
+                precondition_value = precondition[1]
+                if precondition_predicate == reached_predicate \
+                                and precondition_value == value:
+                    continue
         # quando entrar no frontier
         #used_prop[effects[i].get_index()] = True
-        
