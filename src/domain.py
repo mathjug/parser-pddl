0bbd91bdbaf2ae0772d3b7f5db32a89cd53c3b35
Matheus Jurgensen <mjurgensen@usp.br>
2024-07-01 17:51:24 -0300

    Refactoring: improve modularization, change variable names, rearrange methods, and change small implementations.

--- src/domain.py
+++ src/domain.py
@@ -10,28 +10,36 @@ class Domain:
     def __store_actions(self, parsed_domain, stored_predicates):
         actions = []
         pred_to_actions = {}
-        for action in parsed_domain.actions:
-            all_possible_effects = []
-            action_name = action.name
-            parameters = self.__process_action_arguments(action)
-            preconditions = self.__store_preconditions_of_action(action, stored_predicates)
-            action_effect = action.effect
-            self.__store_effects_of_action(action_effect, stored_predicates,
-                                                                  all_possible_effects)
-            effects = self.__merge_effects(all_possible_effects)
-            action = Action(action_name, parameters, preconditions, effects)
+        for parsed_action in parsed_domain.actions:
+            action = self.__build_action_instance(parsed_action, stored_predicates)
             actions.append(action)
             pred_to_actions = self.__store_actions_by_preconditions(action, pred_to_actions)
         return actions, pred_to_actions
+    
+    def __build_action_instance(self, parsed_action, stored_predicates):
+        action_name = parsed_action.name
+        parameters = self.__process_action_arguments(parsed_action)
+        preconditions = self.__store_preconditions_of_action(parsed_action, stored_predicates)
+        all_possible_effects = []
+        action_effect = parsed_action.effect
+        self.__store_effects_of_action(action_effect, stored_predicates, all_possible_effects)
+        effects = self.__merge_effects(all_possible_effects)
+        action = Action(action_name, parameters, preconditions, effects)
+        return action
 
     def __process_action_arguments(self, action):
         parameters = []
         action_arguments = action.parameters
         for argument in action_arguments:
-            argument_name = argument.name
-            argument_type = argument.type_tags
-            parameters.append(Object(argument_name, argument_type))
+            object = self.__build_object_instance(argument)
+            parameters.append(object)
         return parameters
+    
+    def __build_object_instance(self, parsed_object):
+        object_name = parsed_object.name
+        object_type = str(next(iter(parsed_object.type_tags)))
+        object = Object(object_name, object_type)
+        return object
 
     def __store_actions_by_preconditions(self, action, pred_to_actions):
         preconditions = action.get_preconditions()
@@ -56,25 +64,22 @@ class Domain:
     
     def __store_effects_of_action(self, action_effects, stored_predicates, all_possible_effects = []):
         effects_type = str(type(action_effects))
-
         if not hasattr(action_effects, "operands"):
-            proposition_with_bool = self.__store_one_effect_or_precondition_predicate(
+            proposition_with_value = self.__store_one_effect_or_precondition_predicate(
                                                 action_effects, stored_predicates)
-            all_possible_effects.append(proposition_with_bool)
+            all_possible_effects.append(proposition_with_value)
+            return
+        if effects_type == "<class 'pddl.logic.base.OneOf'>":
+            non_deterministic_effect = []
+            for possible_effect in action_effects.operands:
+                scenario = []
+                self.__store_effects_of_action(possible_effect, stored_predicates, scenario)
+                non_deterministic_effect.append(scenario)
+            all_possible_effects.append(non_deterministic_effect)          
         else:
-            if effects_type == "<class 'pddl.logic.base.OneOf'>":
-                non_deterministic_effect = []
-                for possible_effect in action_effects.operands:
-                    scenario = []
-                    self.__store_effects_of_action(possible_effect, stored_predicates,
-                                                            scenario)
-                    non_deterministic_effect.append(scenario)
-                all_possible_effects.append(non_deterministic_effect)
-                 
-            else:
-                for possible_effect in action_effects.operands:
-                    self.__store_effects_of_action(possible_effect, stored_predicates,
-                                                                 all_possible_effects)
+            for possible_effect in action_effects.operands:
+                self.__store_effects_of_action(possible_effect, stored_predicates,
+                                                                all_possible_effects)
 
     def __merge_effects(self, effects):
         deterministic_effects = []
@@ -96,10 +101,9 @@ class Domain:
         pred, bool_value = self.__get_predicate_and_boolean_value(pred)
         predicate = stored_predicates[pred.name]
         objects = []
-        for object in pred.terms:
-            obj_name = object.name
-            obj_type = str(next(iter(object.type_tags)))
-            objects.append(Object(obj_name, obj_type))
+        for parsed_object in pred.terms:
+            object = self.__build_object_instance(parsed_object)
+            objects.append(object)
         proposition = Proposition(predicate, objects)
         proposition_with_bool = (proposition, bool_value)
         return proposition_with_bool
@@ -113,15 +117,13 @@ class Domain:
 
     def __store_constants(self, parsed_domain):
         dict_const = {}
-        for constant in parsed_domain.constants:
-            constant_type = str(next(iter(constant.type_tags)))
+        for parsed_constant in parsed_domain.constants:
+            constant_type = str(next(iter(parsed_constant.type_tags)))
             if constant_type not in dict_const:
                 dict_const[constant_type] = []
-
-            constant_name = repr(constant)[9:-1]
+            constant_name = parsed_constant.name
             constant = Object(constant_name, constant_type)
             dict_const[constant_type].append(constant)
-
         return dict_const
 
     def __store_predicates(self, parsed_domain):
@@ -130,10 +132,8 @@ class Domain:
             variable_types = []
             for object in predicate.terms:
                 variable_types.append(str(next(iter(object.type_tags))))
-
             predicate_object = Predicate(predicate.name, variable_types)
             predicates[predicate.name] = predicate_object
-
         return predicates
 
     def get_constants(self):
@@ -147,25 +147,3 @@ class Domain:
     
     def get_pred_to_actions(self):
         return self.pred_to_actions
-
-def main():
-    domain_path = "../tests/examples/gripper3.pddl"
-    parsed_domain = parse_domain(domain_path)
-    domain = Domain(parsed_domain)
-
-    print("CONSTANTS")
-    constant_dict = domain.constants
-    for constant_type in constant_dict:
-        print(constant_type, end = ':')
-        constants = constant_dict[constant_type]
-        for constant in constants:
-            print(f" {constant}", end = '')
-        print("\n")
-
-    print("PREDICATES")
-    predicates = domain.predicates
-    for predicate in predicates:
-        print(predicate)
-
-if __name__ == "__main__":
-    main()
