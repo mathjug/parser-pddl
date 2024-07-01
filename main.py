b1ea7b5bc7abf4bcd8046337a0fe957d1bc92728
Matheus Jurgensen <mjurgensen@usp.br>
2024-07-01 17:54:21 -0300

    Changes main.py.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from src.domain import Domain
-from pddl import parse_domain
+from src.problem import Problem
+from pddl import parse_domain, parse_problem
 
 def main():
     parsed_domain = parse_domain("tests/examples/triangle-tire.pddl")
+    parsed_problem = parse_problem("tests/examples/triangle-tire-1.pddl")
     domain = Domain(parsed_domain)
+    problem = Problem(parsed_problem)
     actions = domain.get_actions()
     for i, action in enumerate(actions):
         print(action.get_name())
