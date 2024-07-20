a2e14366599bae4a83779ca01e1761b7696c1eac
andre <andrenogueiraribeiro@usp.br>
2024-07-20 17:56:17 -0300

    Finish main

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from src import Parser
+import sys
 
 def main():
-    domain_path = "tests/examples/gripper3.pddl"
-    problem_path = "tests/examples/gripper3_3_balls.pddl"
+    arguments = sys.argv[1:]
+    if len(arguments) != 2:
+        raise Exception("Wrong input format.\nUse: python3 main.py <domain_path> <problem_path>.")
+    
+    domain_path = arguments[0]
+    problem_path = arguments[1]
+    problem_name = problem_path.split(".")[0].split('/')[-1]
+    output_path = problem_name + '.out'
     parser = Parser(domain_path, problem_path)
-    reachable_actions = parser.get_reachable_actions()
-    print(reachable_actions)
+    parser.print_bdds(output_path)
 
 if __name__ == "__main__":
     main()
+
+    # "tests/examples/gripper3.pddl" "tests/examples/gripper3_1_ball.pddl"
